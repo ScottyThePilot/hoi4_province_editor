02dee322d18a28777f6b23ab4b45794214f5f6b5
ScottyThePilot <scotty.codes@gmail.com>
2025-09-15 04:46:45 -0400

    Keep extraneous files in zip archives on save

--- src/util.rs
+++ src/util.rs
@@ -3,11 +3,20 @@ pub mod random;
 pub mod uord;
 
 use chrono::Local;
+use fs_err::File;
 use fxhash::{FxHashMap, FxHashSet, FxBuildHasher};
+use unicase::UniCase;
+use unicase::bytemuck::TransparentWrapper;
 use vecmath::Vector2;
+use zip::read::ZipArchive;
+use zip::result::ZipError;
+use zip::write::ZipWriter;
 
 use crate::app::map::{Color, Extents};
+use crate::error::Error;
 
+use std::io::{self, prelude::*};
+use std::path::{Path, PathBuf};
 use std::ops::Range;
 
 #[derive(Debug, Clone)]
@@ -91,4 +100,80 @@ macro_rules! try_block {
       $($t)*
     })()
   };
+}
+
+
+
+#[derive(Debug, Clone)]
+pub struct ZipFilesMap {
+  map: FxHashMap<UniCase<PathBuf>, Vec<u8>>
+}
+
+impl ZipFilesMap {
+  pub fn new() -> Self {
+    ZipFilesMap { map: FxHashMap::default() }
+  }
+
+  pub fn with_capacity(capacity: usize) -> Self {
+    ZipFilesMap { map: fx_hash_map_with_capacity(capacity) }
+  }
+
+  pub fn get(&self, name: impl AsRef<Path>) -> Option<&Vec<u8>> {
+    self.map.get(UniCase::wrap_ref(name.as_ref()))
+  }
+
+  pub fn get_mut(&mut self, name: impl AsRef<Path>) -> Option<&mut Vec<u8>> {
+    self.map.get_mut(UniCase::wrap_ref(name.as_ref()))
+  }
+
+  pub fn get_or_insert(&mut self, name: impl Into<PathBuf>) -> &mut Vec<u8> {
+    self.map.entry(UniCase::wrap(name.into())).or_default()
+  }
+
+  pub fn get_insert_new(&mut self, name: impl Into<PathBuf>) -> &mut Vec<u8> {
+    self.map.entry(UniCase::wrap(name.into())).and_modify(Vec::clear).or_default()
+  }
+
+  pub fn remove(&mut self, name: impl AsRef<Path>) -> Option<Vec<u8>> {
+    self.map.remove(UniCase::wrap_ref(name.as_ref()))
+  }
+
+  pub fn from_reader(reader: impl Read + Seek) -> Result<Self, ZipError> {
+    let mut zip_reader = ZipArchive::new(reader)?;
+    let mut zip_files_map = ZipFilesMap::with_capacity(zip_reader.len());
+    for i in 0..zip_reader.len() {
+      let mut zip_file = zip_reader.by_index(i)?;
+      if let Some(zip_file_name) = zip_file.enclosed_name().map(Path::to_owned) {
+        let zip_file_buffer = zip_files_map.get_insert_new(zip_file_name);
+        io::copy(&mut zip_file, zip_file_buffer).map_err(ZipError::Io)?;
+      };
+    };
+
+    Ok(zip_files_map)
+  }
+
+  pub fn to_writer(&self, writer: impl Write + Seek, comment: impl Into<String>) -> Result<(), ZipError> {
+    let mut zip_writer = ZipWriter::new(writer);
+    zip_writer.set_comment(comment);
+
+    for (zip_file_name, zip_file_buffer) in self.map.iter() {
+      let zip_file_name = AsRef::<Path>::as_ref(zip_file_name).to_string_lossy();
+      zip_writer.start_file(zip_file_name, Default::default())?;
+      zip_writer.write_all(zip_file_buffer).map_err(ZipError::Io)?;
+    };
+
+    zip_writer.finish()?;
+
+    Ok(())
+  }
+
+  pub fn edit<F>(mut source: File, comment: impl Into<String>, callback: F) -> Result<(), Error>
+  where F: FnOnce(&mut Self) -> Result<(), Error> {
+    let mut zip_files_map = Self::from_reader(&mut source)?;
+    source.seek(io::SeekFrom::Start(0)).map_err(ZipError::Io)?;
+    source.set_len(0).map_err(ZipError::Io)?;
+    callback(&mut zip_files_map)?;
+    zip_files_map.to_writer(&mut source, comment)?;
+    Ok(())
+  }
 }
